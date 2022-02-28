7c6d2da8f4e83abe024a97b6a24166f929402d5c
DYMO Software <57638744+dymosoftware@users.noreply.github.com>
2022-02-28 09:59:32 -0500

    Add a Visual Basic SDK Sample

---
+++ VisualBasic_Sample/VB_DYMOSDK_Sample/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports DymoSDK.Implementations
+
+Module Module1
+
+    Sub Main()
+        DymoSDK.App.Init()
+        Dim dymoSDKLabel = DymoLabel.Instance
+        Dim Printers = DymoPrinter.Instance.GetPrinters()
+        For Each prt In Printers    ' Iterate through each element.  
+            Console.WriteLine($"Printer info: {prt.DriverName}, {prt.Name}, {prt.IsTwinTurbo}")
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ VisualBasic_Sample/VB_DYMOSDK_Sample/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VB_DYMOSDK_Sample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VB_DYMOSDK_Sample")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bddc6c55-33dc-4d90-bb8d-790d8f34b774")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
